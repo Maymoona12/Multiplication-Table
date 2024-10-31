7341fcc5b75d4121785b8c29f634921eab5a9e82
Maymoona <ayoumaymoona.a.b@gmail.com>
2024-10-31 13:33:47 +0200

    Add Multiply Function

--- Assets/Scripts/MultiplicationTable.cs
+++ Assets/Scripts/MultiplicationTable.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 public class MultiplicationTable : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Start(){
+    for (int i =1 ; i < 11; i++)
     {
-    
+     Multiply(5,i);  
+    }
+    }
+
+    int Multiply(int number1 , int number2){
+    int result = 0;
+    result = number1 * number2 ;
+    Debug.Log( number1 + "*" + number2 + "=" + result);
+    return result ;
     }
 }
